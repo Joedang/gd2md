1261d47b703850fa0815a297d8bf27694d1bb6e9
Joedang <shields6@pdx.edu>
2018-03-05 22:16:04 -0800

    wrote a script to mass download docs

--- docsSOPs/notes.md
+++ docsSOPs/notes.md
@@ -3,11 +3,17 @@
 ## Overview
 00. people paste "share" links from google docs into `SOPlinks.txt`
 10. extract the Google Drive ID from the shareable link
+	```
+	cat SOPlinks.txt | grep -P -o -e '(?<=id=)[[:alnum:]_-]*$'
+	```
 20. use the drive ID to create a "download as docx" URL
+	- Okay
 30. grab the docx via wget and put it in $DRIVEID.docx
 40. get the document title and rename $DRIVEID.docx to $DOCTITLE.docx
-	- pandoc asdf.docx --template=../titleOnly.md -o asdf.title
-	- cat asdf.title | sed -e 's/<span.*span>//'
+	```
+	pandoc asdf.docx --template=../titleOnly.md -o asdf.title
+	cat asdf.title | sed -e 's/<span.*span>//'
+	```
 	- Actually, pandoc seems to go through the whole conversion anyways to get the title.
 	  So, I could simply filter out the proper title after the conversion to markdown.
 50. use pandoc to convert to $DOCTITLE.md with the media in $DOCTITLE/
---
+++ docsSOPs/scrapeDocx.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# scrape a bunch of DOCX files from a list of google docs share links
+# take names of files containing lists of GD share links
+# download them to files named after their GDID
+# 2018-03-05
+# Joe Shields
+
+# prefix to request a GD as DOCX
+docxPrefix='https://docs.google.com/document/u/1/d/'
+
+# suffix 
+docxSuffix='/export?format=docx'
+
+# extract the Googoo Drive IDs from the list files given:
+GDIDS=`cat "$@" | grep -P -o -e '(?<=id=)[[:alnum:]_-]*$'`
+
+echo links given: 
+cat "$@"
+echo
+echo Google Drive IDs: 
+echo $GDIDS
+echo
+
+fails=0
+for id in $GDIDS
+do
+	docxLink=$docxPrefix$id$docxSuffix
+	echo
+	echo DOWNLOADING: $docxLink 
+	# don't wuss-out if there's an error
+	set +e 
+	{ # try
+	wget "$docxLink" --output-document=docx/$id'.docx'
+	#echo normally I\'d download here...
+	} || { # catch
+	echo WARNING: $id failed to download.
+	fails=$((fails + 1))
+	echo
+	}
+	echo
+done
+echo done scraping
+echo $fails links failed to download
