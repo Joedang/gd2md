9a43fe8dd32c4cd9ed2ffdcab3a787e444d3c381
Joedang <shields6@pdx.edu>
2018-03-07 20:45:54 -0800

    conversions to PDF; command to deploy to the wiki

--- docsSOPs/docx2md.sh
+++ docsSOPs/docx2md.sh
@@ -5,5 +5,5 @@ do
 	f_name="${f_base%.*}" # no path or extension, just the name
 	f_noext="${f%.*}" # name and path, no extension
 	echo converting $f to $f_noext'.md'
-	pandoc $f --template=template.md --extract-media=downloads/$f_name/ -o $f_noext'.md'
+	pandoc $f --template=template.md --extract-media=downloads/$f_name -o $f_noext'.md'
 done
