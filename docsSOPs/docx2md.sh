fd02d98ddd62925329ae9d1908f3b3b7121778b3
Joedang <shields6@pdx.edu>
2018-03-07 21:10:58 -0800

    fixed deployment stuff

--- docsSOPs/docx2md.sh
+++ docsSOPs/docx2md.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-for f in $@
+DOCSDIR=$1
+OLDDIR=`pwd`
+cd $DOCSDIR
+for f in *.docx
 do
 	f_base=$(basename $f) # name and extension, no path
 	f_name="${f_base%.*}" # no path or extension, just the name
 	f_noext="${f%.*}" # name and path, no extension
 	echo converting $f to $f_noext'.md'
-	pandoc $f --template=template.md --extract-media=downloads/$f_name -o $f_noext'.md'
+	pandoc $f --template=$OLDDIR/template.md --extract-media=$f_name -o $f_noext'.md'
 done
+cd $OLDDIR
