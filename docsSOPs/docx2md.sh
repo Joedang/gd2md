f87eb9d8ed4a9db0ce7b9615cc3d3d1be1885c17
Joedang <shields6@pdx.edu>
2018-03-07 19:49:57 -0800

    cleaned up some variable names

--- docsSOPs/docx2md.sh
+++ docsSOPs/docx2md.sh
@@ -2,8 +2,8 @@
 for f in $@
 do
 	f_base=$(basename $f) # name and extension, no path
-	f_id="${f_base%.*}" # no path or extension, just the GDID
+	f_name="${f_base%.*}" # no path or extension, just the name
 	f_noext="${f%.*}" # name and path, no extension
 	echo converting $f to $f_noext'.md'
-	pandoc $f --template=template.md --extract-media=docx/ -o $f_noext'.md'
+	pandoc $f --template=template.md --extract-media=downloads/$f_name/ -o $f_noext'.md'
 done
--- docsSOPs/scrapeDocx.sh
+++ docsSOPs/scrapeDocx.sh
@@ -15,7 +15,7 @@ docxSuffix='/export?format=docx'
 GDIDS=`cat "$@" | grep -P -o -e '(?<=id=)[[:alnum:]_-]*$'`
 
 # dowload directory
-DLdir=docx/
+DLdir=downloads/
 
 # header directory
 Hdir=headers/
@@ -28,6 +28,7 @@ echo $GDIDS
 echo
 
 fails=0
+errors=0
 for id in $GDIDS
 do
 	docxLink=$docxPrefix$id$docxSuffix
@@ -36,18 +37,26 @@ do
 	# don't wuss-out if there's an error
 	set +e 
 	{ # try
-	curl -L -D $Hdir$id.header "$docxLink" -o $DLdir$id'.download'
-	if ! grep -i Error $Hdir$id.header;# if the server didn't give errors, rename the download
-	then
-		filename=`cat $id.header | grep -P -o -e '(?<=filename=").*(?=")'`
-		echo I would like to move $DLdir$id.download to $DLdir$filename
-	fi
+		# capture both the header and the file from the HTTP response
+		curl -L -D $Hdir$id.header "$docxLink" -o $DLdir$id'.download'
+		# if the server didn't give errors, rename the download
+		if ! grep -i Error $Hdir$id.header;
+		then
+			# grab the filename from the HTTP response header
+			filename=`cat $Hdir$id.header | grep -P -o -e '(?<=filename=").*(?=")'`
+			# rename the download
+			echo I\'m moving $DLdir$id.download to $DLdir$filename
+			mv $DLdir$id.download $DLdir$filename
+		else
+			errors=$((errors + 1))
+		fi
 	} || { # catch
-	echo WARNING: $id failed to download.
-	fails=$((fails + 1))
+		echo WARNING: $id failed to download.
+		fails=$((fails + 1))
 	echo
 	}
 	echo
 done
 echo done scraping
 echo $fails links failed to download
+echo $errors links had errors when downloading
