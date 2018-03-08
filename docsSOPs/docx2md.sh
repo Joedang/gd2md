49c4de57196ee12263da0792126a239660ddb699
Joedang <shields6@pdx.edu>
2018-03-07 21:45:38 -0800

    changed media location to match existing scheme

--- docsSOPs/docx2md.sh
+++ docsSOPs/docx2md.sh
@@ -8,6 +8,6 @@ do
 	f_name="${f_base%.*}" # no path or extension, just the name
 	f_noext="${f%.*}" # name and path, no extension
 	echo converting $f to $f_noext'.md'
-	pandoc $f --template=$OLDDIR/template.md --extract-media=$f_name -o $f_noext'.md'
+	pandoc $f --template=$OLDDIR/template.md --extract-media=images/$f_name -o $f_noext'.md'
 done
 cd $OLDDIR
